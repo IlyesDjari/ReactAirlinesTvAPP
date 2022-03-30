028479183eefb9c4a90894b4b0f82dd9459fa635
IlyesDjari <71768687+IlyesDjari@users.noreply.github.com>
2022-03-30 14:00:42 +0200

    WIP Search function added

--- reactairlines/src/component/Programs.tsx
+++ reactairlines/src/component/Programs.tsx
@@ -1,24 +1,34 @@
-import current from './CallApi'
+import current from '../hooks/CallApi'
 import '../layout/programs.scss'
+
+console.log(current);
 
 
 function Programs() {
-    return ( 
+
+
+  function sayHello(program: any) {
+  console.log("hello");
+  }
+  
+    return (
       <div id='fullProgram'>
          {current.map((program: any) => (
     <div id='program'>
-      <><img id='coverimg' key={program.show.image.medium} src={program.show.image.medium} alt="" />
+      <button id='buttonDetail'  >
+      <><img id='coverimg'src={program.show.image.medium} alt="" />
       <div id='info'>
-          <h1 id='airtime' key={program.airtime} >{program.airtime}</h1> 
-          <h1 id='duration'>{program.show.runtime} minutes</h1>
-          <h1 id='genre' key={program.show.type}>{program.show.type}</h1>
-          <h2 key={program.show.name}>{program.show.name} - {program.name}</h2>
-          <p key={program.show.summary} dangerouslySetInnerHTML={{__html:program.show.summary}}></p>
+          <h1 id='airtime' >{program.airtime}</h1> 
+          <h1 id='duration' >{program.show.runtime} minutes</h1>
+          <h1 id='genre'>{program.show.type}</h1>
+          <h2 >{program.show.name} - {program.name}</h2>
+          <p  dangerouslySetInnerHTML={{__html:program.show.summary}}></p>
         </div></> 
+        </button>
     </div>
      ))}
   </div>
     )
   }
-
+  
   export default Programs
---
+++ reactairlines/src/hooks/CallApiSearch.ts
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import { Series, Show } from '../model/series';
+import Axios from 'axios';
+
+export const getSearch = () => {
+
+  const [result, setResult] = useState<Array<Series>>([]);
+  const [details, setDetails] = useState<Show>();
+
+  const searchHandler = (text: string) => {
+    Axios.get<Array<Series>>(`http://api.tvmaze.com/search/shows?q=${ text }`).then(
+      res => {
+        setResult(res.data);
+      }
+    );
+  }
+
+  const itemClickHandler = (series: Series) => {
+    setDetails(series.show)
+  };
+  
+  return {
+    series: result,
+    search: searchHandler,
+    itemClick: itemClickHandler,
+    details,
+  }
+}
